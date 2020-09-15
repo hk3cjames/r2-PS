6b6315c093b0e6541e42b4074e24149fa6c7c42e
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-09-15 23:14:08 +0800

    rbas program

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+app.listen(3000, () => console.log("PS-1001 listening at 127.0.0.1:3000"));
+app.use(express.static("ts"));
+app.use(express.json({ limit: "1mb" }));
+
+app.post("/hashFile", (req, res) => {
+  console.log(req.body);
+  time = Date.now();
+  res.json({
+    status: "success",
+    tick: time
+  });
+});
