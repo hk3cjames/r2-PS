3856c4544f489199a4cfd9ef773bb44de0524009
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-09-16 23:22:23 +0800

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 app.listen(3000, () => console.log("PS-1001 listening at 127.0.0.1:3000"));
 app.use(express.static("ts"));
+
 app.use(express.json({ limit: "1mb" }));
 
 app.post("/hashFile", (req, res) => {
