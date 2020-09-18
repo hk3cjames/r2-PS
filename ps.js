3ae2f695933666b53d3bb40ccb31d5a70d62aa01
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-09-19 01:16:25 +0800

    add cors

--- ps.js
+++ ps.js
@@ -1,12 +1,7 @@
 // input hash from ts to PS, upload to DB-c port 3100
 const express = require("express");
-const cors = require('cors');
-
 const app = express();
-app.use(cors())
-
 app.listen(3100, () => console.log("PS-1001 listening at 127.0.0.1:3100"));
-app.use(express.static("ts"));
 
 app.use(express.json({ limit: "1mb" }));
 
