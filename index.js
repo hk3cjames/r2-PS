afcff220eb3c0192f842f557894be915d7e22336
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-09-20 19:05:00 +0800

    first PS run on pi 4 with node-fetch

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 // input hash from ts to PS, upload to DB-c port 3100
 const express = require("express");
 const cors = require('cors');
-
 const app = express();
 app.use(cors())
 
+const fetch = require("node-fetch");
+
 app.listen(3100, () => console.log("PS-1001 listening at 127.0.0.1:3100"));
-app.use(express.static("ts"));
 
 app.use(express.json({ limit: "1mb" }));
 
+var res1 = {};
+existHash = "ab1234";
 app.post("/hashFile", (req, res) => {
-  console.log(req.body);
+  rxjson = req.body;
+  console.log(rxjson);
+
   time = Date.now();
+  newHash = rxjson.thash;
+
+  console.log(existHash);
+  console.log(newHash);
+
+  if (newHash === existHash) {
+    res_status = "fail";
+  } else {
+    res_status = "success";
+    existHash = rxjson.thash;
+  }
+  console.log(res_status)
   res.json({
-    status: "success",
+    status: res_status,
+    hash: existHash,
     tick: time
   });
+
 });
