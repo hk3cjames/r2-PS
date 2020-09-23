7223ab946b8dd27960408df2018d1512f238d804
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-09-23 22:57:31 +0800

    add db-c

--- index.js
+++ index.js
@@ -11,28 +11,35 @@ app.listen(3100, () => console.log("PS-1001 listening at 127.0.0.1:3100"));
 app.use(express.json({ limit: "1mb" }));
 
 var res1 = {};
-existHash = "ab1234";
-app.post("/hashFile", (req, res) => {
+existHash = "to be confirm in future cycle";
+result = "next"
+app.post("/hashFile", async (req, res) => {
   rxjson = req.body;
   console.log(rxjson);
 
   time = Date.now();
   newHash = rxjson.thash;
-
-  console.log(existHash);
   console.log(newHash);
+  console.log(existHash);
 
-  if (newHash === existHash) {
-    res_status = "fail";
-  } else {
-    res_status = "success";
-    existHash = rxjson.thash;
-  }
-  console.log(res_status)
   res.json({
-    status: res_status,
+    status: result,
     hash: existHash,
-    tick: time
+    tick: time,
   });
 
+  data = rxjson;
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+  res1 = await fetch("http://127.0.0.1:3200/hashFile", options)
+  const disp = await res1.json();
+  console.log(disp)
+  console.log(disp.status)
+  result = disp.status
+existHash = disp.hash
 });
