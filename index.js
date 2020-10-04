7eccd2de9250c3b92d977c5920b006e4134f3184
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-10-04 19:58:24 +0800

    sync to one drive

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 app.use(cors())
-
 const fetch = require("node-fetch");
 
-app.listen(3100, () => console.log("PS-1001 listening at 127.0.0.1:3100"));
-
+app.listen(3100, () => 
+  console.log("PS listening at 127.0.0.1:3100"+
+  "\n"+"connect to DB-C at :3200"));
 app.use(express.json({ limit: "1mb" }));
 
 var res1 = {};
 existHash = "to be confirm in future cycle";
 result = "next"
+
 app.post("/hashFile", async (req, res) => {
   rxjson = req.body;
   console.log(rxjson);
@@ -29,6 +30,7 @@ app.post("/hashFile", async (req, res) => {
   });
 
   data = rxjson;
+  
   const options = {
     method: "POST",
     headers: {
@@ -36,10 +38,11 @@ app.post("/hashFile", async (req, res) => {
     },
     body: JSON.stringify(data),
   };
-  res1 = await fetch("http://127.0.0.1:3200/hashFile", options)
+
+  res1 = await fetch("http://127.0.0.1:3200/hashFile", options) //to DB_c
   const disp = await res1.json();
   console.log(disp)
   console.log(disp.status)
   result = disp.status
-existHash = disp.hash
+  existHash = disp.hash
 });
