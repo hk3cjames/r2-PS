a160fb77a2b7e8799c478d325c91101b4452402e
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-10-26 22:20:02 +0800

    update with load simulator

--- index.js
+++ index.js
@@ -7,56 +7,135 @@ app.use(cors());
 const fetch = require("node-fetch");
 app.use(express.static("ps"));
 app.listen(3100, () =>
-  console.log("PS listening at port 3100" + "\n" + "connect to DB-C at :3200"));
+  console.log("PS listening at port 3100" + "\n" + "connect to DB-C at :3200")
+);
 app.use(express.json({ limit: "1mb" }));
 
 var res1 = {};
-let tsKey = [];
+tsKeyOriginal = [];
+tsKey = [];
 tsName = [];
+tsIn = [];
+tsRdy = [];
+dashboardDisp = [];
+linkHash = "";
+displayCount = 0;
+nextChain = "";
+busyFlag = false;
+
 let i = 0;
 let j = 0;
 let k = 0;
+
 for (i = 0; i < 1000; i++) {
   k = 1000 + i;
-  j = "BRAS.ps1000.ts" + k;
+  j = "" + k;
   tsName[i] = j;
   tsKey[i] = crypto.createHash("sha256").update(j).digest("hex");
+  tsKeyOriginal[i] = tsKey[i];
+  tsIn[i] = "abcd1234567890abcdef";
+  tsRdy[i] = true;
 }
 
 console.log(tsName);
 console.log(tsKey);
+hashCount = 0;
+tickCounter = 0;
+result = 0;
 
-existHash = "to be confirm in future cycle";
-result = "next";
+function checkTSname(name, ID) {
+  for (i = 0; i < 1000; i++) {
+    if (name == tsName[i]) {
+      if (ID == tsKey[i] || ID == tsKeyOriginal[i]) {
+        return i;
+      }
+    }
+  }
+  return 1000; // >= 1000
+}
+
+async function timeAdj() {
+  return;
+}
 
 app.post("/hashFile", async (req, res) => {
   rxjson = req.body;
-  console.log(rxjson);
+  // console.log("TS input json");
+  // console.log(rxjson)
 
-  time = Date.now();
-  newHash = rxjson.thash;
-  console.log(newHash);
-  console.log(existHash);
+  while (busyFlag == true) {
+    setInterval(timeAdj, 10);
+  }
+  tsPointer = checkTSname(rxjson.tsId, rxjson.chainId);
+  // console.log("tsId =" + rxjson.tsId) //==index of input hash
+  // console.log("tsPointer =" + tsPointer)
 
-  res.json({
+  if (tsPointer < 1000) {
+    tsIn[tsPointer] = rxjson.tHash;
+    tsKey[tsPointer] = rxjson.tHash;
+    result = "submitted";
+  } else {
+    resullt = "fail: wrong name+ID";
+  }
+
+  resJson = {
     status: result,
-    hash: existHash,
-    tick: time,
-  });
-
-  data = rxjson;
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
+    nextChainId: nextChain,
+    tick: tickCounter,
   };
-
-  res1 = await fetch("http://127.0.0.1:3200/hashFile", options); //to DB_c
-  const disp = await res1.json();
-  console.log(disp);
-  console.log(disp.status);
-  result = disp.status;
-  existHash = disp.hash;
+  res.json(resJson);
+  // console.log("PS response to TS");
+  // console.log(resJson);
 });
+
+app.post("/PSdisplay", async (req, res) => {
+  // input from PS dashboard
+  rxjson = req.body;
+  // console.log("PS dashboard req");
+  // console.log(rxjson);
+  while (busyFlag == true) {
+    setInterval(timeAdj, 10);
+  }
+  // console.log("PS response to TS");
+
+  resJson = {
+    cycle: tickCounter,
+    cycleHashCount: displayCount,
+    cycleLinkHash: linkHash,
+    dashboardDisp,
+  };
+  res.json(resJson);
+  // console.log(resJson);
+
+  cycleLinkHash = tsKey[0];
+});
+
+async function intervalFunc() {
+  busyFlag = true;
+  tickCounter++;
+  // data = rxjson;
+  // const options = {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(data),
+  // };
+  // res1 = await fetch("http://127.0.0.1:3200/hashFile", options); //to DB_c
+  // const DBcDisp = await res1.json();
+  // console.log(DBcDisp);
+
+  // result = disp.status;
+  // existHash = disp.hash;
+
+  // console.log(tsIn);
+  displayCount = 0
+  for (i = 0; i < 1000; i++) {
+    if (tsIn[i] !=="") {displayCount++}
+    dashboardDisp[i] = tsIn[i];
+    tsIn[i] = "";
+  }
+  
+  busyFlag = false;
+}
+setInterval(intervalFunc, 1000);
--- ps/ps.js
+++ ps/ps.js
@@ -1,24 +1,43 @@
-var tickCounter = 0;
+
 var time = 0;
 var hashCountr = 0;
+var PsId = "1000";
+var tsHash = []
 
 
-function intervalFunc() {
-  document.getElementById("tick").textContent = tickCounter;
-  tickCounter++;
+async function intervalFunc() {
+
+ 
+
+
+  const data = { PsId };
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  };
+
+  const res = await fetch("/PSdisplay", options);
+  const disp = await res.json();
+  // console.log(disp);
+
+  document.getElementById("tick").textContent = disp.cycle;
   const d = new Date();
-  var str = ''+ d
-  var test = str.substring(4,24)
+  var str = "" + d;
+  var test = str.substring(4, 24);
+
   document.getElementById("time").textContent = test;
+  document.getElementById("count").textContent = disp.cycleHashCount;
+  document.getElementById("hash").textContent = disp.cycleLinkHash;
+
   for (i = 1000; i < 2000; i++) {
-    display = "" + i + "- " 
-    j=i-1000
-    document.getElementsByTagName("p")[j].textContent = display.substring(1);
+    j = i - 1000;
+    k = disp.dashboardDisp[j]
+    var hashDisp = "" + k
+    // console.log(hashDisp)
+    display = "" + i + "- "  + hashDisp
+    document.getElementsByTagName("p")[j].textContent = display.substring(1,12);
   }
-    document.getElementsByTagName("p")[2].textContent = display + 123456;   
-    document.getElementsByTagName("p")[4].textContent = display+ tickCounter;   
-    document.getElementsByTagName("p")[6].textContent = display+ tickCounter;   
-    document.getElementsByTagName("p")[9].textContent = display+ tickCounter;   
 }
 
-setInterval(intervalFunc, 1000);
+setInterval(intervalFunc, 900);
